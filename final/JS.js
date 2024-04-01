883fa312bfdeda383a34243c2671f2c54e21fba9
DerickCJ <155499024+DerickCJ@users.noreply.github.com>
2024-04-01 08:57:28 -0400

    Add files via upload

---
+++ final/JS.js
@@ -0,0 +1,43 @@
+
+// Global header and footer
+
+document.getElementById("logo").innerHTML = `<img src="./images/logo.png" alt="logo">`;
+
+document.getElementById("menu").innerHTML = `
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Product</a></li>
+      <li><a href="#">About us</a></li>
+      <li><a href="#">Contact Us</a></li>`;
+
+document.getElementById("login").innerHTML = `
+      <a href="#">Login</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="#">Register</a>`;
+
+document.getElementById("footer").innerHTML = `
+      &copy; 2024 Rotary Dial. All rights reserved.<br>
+      Privacy Policy`;
+
+
+
+// Navigation color change function
+window.onscroll = function () {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.querySelector("nav").style.backgroundColor = "rgba(0, 0, 0, 0.85)";
+  } else {
+    document.querySelector("nav").style.backgroundColor = "rgba(0, 0, 0, 0)";
+  }
+};
+
+// Images slideshow function
+let count = 0;
+const image = document.querySelector('#pictureShow');
+const totalImage = 2;
+
+function nextImage() {
+  count++;
+  if (count >= totalImage) {
+    count = 0;
+  }
+  image.style.transform = `translateX(${-count * 100}%)`;
+}
+
+setInterval(nextImage, 4000);
